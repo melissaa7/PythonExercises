93f15b3dc203a4ed3e3f4056edf5b6bc7804cbbc
melissaa7 <21457@students.mrgs.school.nz>
2024-03-04 20:31:51 +1300

    class

--- Class.py
+++ Class.py
@@ -1,2 +1,15 @@
 from tkinter import*
 
+
+
+
+
+
+
+# Starting point of program #
+if __name__ == "__main__":
+  root = Tk()
+  root.title("NZ Road Rules Quiz")
+  #quizStarter_object
+  root.mainloop()
+  
