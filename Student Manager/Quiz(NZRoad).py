34814a83e68182ee3aded9df6b355d14a757c14e
melissaa7 <21457@students.mrgs.school.nz>
2024-03-13 20:57:07 +1300

    part3

--- Student Manager/Quiz(NZRoad).py
+++ Student Manager/Quiz(NZRoad).py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *
 
 names_list = []
@@ -6,9 +7,9 @@ asked = []
 
 questions_answers = {
     1: ["What must you do when you see blue and red flashing lights behind you?",
-        'Speed up to get out of theway','Slow down and drive carefully',
+        'Speed up to get out of the way','Slow down and drive carefully',
         'Slow down and stop','Drive on as usual','Slow down and stop',3],
-    2: ["You may stop on motorwayonly if:",'if there is an emergency',
+    2: ["You may stop on motorway only if:",'if there is an emergency',
         'To let down or pick up passengers','to make U-turn',
         'To stop and take a photo','If there is an emegency',1],
     3: ["When coming up to a pedestrian crossing without raised traffic island, what must you do?",
@@ -21,14 +22,14 @@ questions_answers = {
     5: ["What is the maximum speed you may drive if you have a 'space saver wheel' fitted? (km/h)",
         '70 km/h','100 km/h so you do not hold up traffic','80 km/h and if the wheel spacer displays a lower limit that applies',
         '90 km/h','80 km/h and if the wheel spacer displays a lower limit that applies',3],
-    6: ["When following another vehicleon a dusty road, you should:",'Speed up to get passed',
+    6: ["When following another vehicle on a dusty road, you should:",'Speed up to get passed',
         "Turn your vehicle's windscreen wipers on",'Stay back from the dust cloud',
         'Turn your vehicles headlights on', 'Stay back from the dust cloud',3],
     7: ["What does the sign containing the letters 'LSZ' mean",'Low Safety zone', 'Lone star zone', 'Limited speed zone',
         'Limited speed zone',4],
     8: ["What speed are you allowed to pass a school bus that has stopped get on or off?",'20 km/h',
         '30 km/h','70 km/h','10 km/h','20 km/h',1],
-    9: ["What is the maximum distance a load maqy extend in front of a car?",'2 meters forward of the front edge of the front seat',
+    9: ["What is the maximum distance a load may extend in front of a car?",'2 meters forward of the front edge of the front seat',
         '4 meters forward of the front edge of the front seat','3 meters forward of the front seat','2.5 meters forward of the front edge of the front seat',
         '3 meters forward of the front edge of the front seat',3],
     10: ["To avoid being blinded by the headlights of another vehicle coming towards you, what should you do?",
@@ -38,10 +39,10 @@ questions_answers = {
 }
 
 def randomiser():
-    global qnum
+    global qnum #The question number is the key in our dictionary questions_answers, we have 10 keys
     qnum = random.randint(1,10)
     if qnum not in asked:
-        asked.apend(qnum)
+        asked.append(qnum)
     elif qnum in asked:
         randomiser()
 
@@ -74,7 +75,45 @@ class QuizStarter:
         names_list.append(name)
         print(names_list)
         self.quiz_frame.destroy()
+        Quiz(root)
+
+class Quiz:
+    def __init__(self, parent):
+        background_color="OldLace"
+        #frame set up
+        self.quiz_frame = Frame(parent,bg = background_color, padx=100, pady=100)
+        self.quiz_frame.grid()
+
+        #randomiser will randomly pick a question number which is qnum
+        randomiser()
+
+        #Label widget for our heading
+        self.question_label = Label (self.quiz_frame, text = questions_answers[qnum][0], font=("Tw cen MT", "18", "bold"), bg=background_color)
+        self.question_label.grid(row=0, padx=10, pady=10)
+
+        #holds the value of radio buttons
+        self.var1=IntVar()
+
+        #first radio button to hold first choice answer 1
+        self.rb1 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][1], font=("Helvetica", "12"), bg="sandybrown", value=1,variable=self.var1, pady=10,padx=10)
+        self.rb1.grid(row=1, sticky=W)
         
+        #first radio button to hold first choice answer 2
+        self.rb2 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][2], font=("Helvetica", "12"), bg="sandybrown", value=2,variable=self.var1, pady=10,padx=10)
+        self.rb2.grid(row=2, sticky=W)
+
+        #first radio button to hold first choice answer 3
+        self.rb3 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][3], font=("Helvetica", "12"), bg="sandybrown", value=3,variable=self.var1, pady=10,padx=10)
+        self.rb3.grid(row=3, sticky=W)
+
+        #first radio button to hold first choice answer 4
+        self.rb4 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][4], font=("Helvetica", "12"), bg="sandybrown", value=4,variable=self.var1, pady=10,padx=10)
+        self.rb4.grid(row=4, sticky=W)
+
+        #confirm answer button
+        self.confirm_button = Button(self.quiz_frame, text="Confirm", bg="pink")
+        self.confirm_button.grid(row=5, sticky=W)
+
 
 
 #Starting Point of the Page #
